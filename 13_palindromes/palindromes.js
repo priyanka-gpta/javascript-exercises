e6979d9711d67576336d3649b3386fb4b04aee99
priyanka-gpta <61194307+priyanka-gpta@users.noreply.github.com>
2025-07-14 20:55:11 +0530

    finished objects exercises

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const cleanString = string.toLowerCase().split('').filter((character => alphanumerical.includes(character))).join('');
+    const reverseString = cleanString.toLowerCase().split('').reverse().join('');
 
+    return cleanString === reverseString;
 };
 
 // Do not edit below this line
--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(arg) {
+    let count;
+    if (typeof arg !== 'number') {
+        count = parseInt(arg);
+    } else {
+        count = arg;
+    }
 
+    if (count < 0) {
+        return "OOPS";
+    }
+    if (count == 0) return 0;
+
+    let a = 1;
+    let b = 0;
+    for (let i = 2; i<=count; i++) {
+        let current = a + b;
+        b = a;
+        a = current;
+    }
+    return a;
 };
 
 // Do not edit below this line
